f2c3bff49472784a84c30a92f972a7c1cd4c5628
mengliYuan <1600708467@qq.com>
2024-11-04 21:02:04 +0800

    Redis:基于Redis实现共享session短信登录

---
+++ src/main/java/com/yml/interceptor/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package com.yml.interceptor;
+
+import com.yml.utils.UserHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
+            // 没有，拦截，设置状态码
+            response.setStatus(401);
+            // 拦截
+            return false;
+        }
+        //6. 有用户放行
+        return true;
+    }
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
