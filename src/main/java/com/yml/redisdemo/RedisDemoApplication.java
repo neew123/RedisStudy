6544e4e822dc8517d7e18274b921c2a9b3996efa
mengliYuan <1600708467@qq.com>
2024-12-23 14:49:55 +0800

    redisTemplate的哨兵模式

--- src/main/java/com/yml/redisdemo/RedisDemoApplication.java
+++ src/main/java/com/yml/redisdemo/RedisDemoApplication.java
@@ -1,9 +1,13 @@
 package com.yml.redisdemo;
 
+import io.lettuce.core.ReadFrom;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 
 
 @EnableAspectJAutoProxy(exposeProxy = true)
@@ -13,6 +17,12 @@ public class RedisDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisDemoApplication.class, args);
+    }
+
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
     }
 
 }
